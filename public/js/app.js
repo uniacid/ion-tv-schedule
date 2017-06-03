473d8d8949c9582cc90c3126223d8239b327acec
Steven Gonzalez <uniacid@gmail.com>
2017-06-03 03:37:06 -0400

    Many updates

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,129 @@
-// import Datepicker from './components/Datepicker.vue';
-
 Vue.prototype.$http = axios;
 
-new Vue ({
+var fullDateToday = new Date();
+var fullDateTomorrow = new Date();
+var fullDateNextWeek = new Date();
+var d = new Date();
+fullDateTomorrow.setDate(fullDateTomorrow.getDate() + 1);
+fullDateTomorrow.setHours(0,0,0,0);
+fullDateNextWeek.setDate(fullDateNextWeek.getDate() + 7);
+var twoDigitMonth = fullDateToday.getMonth()+1+"";if(twoDigitMonth.length==1)	twoDigitMonth="0" +twoDigitMonth;
+var twoDigitMonthNextweek = fullDateToday.getMonth()+1+"";if(twoDigitMonthNextweek.length==1)	twoDigitMonthNextweek="0" +twoDigitMonthNextweek;
+var twoDigitDate = fullDateToday.getDate()+"";if(twoDigitDate.length==1)	twoDigitDate="0" +twoDigitDate;
+var twoDigitDateNextWeek = fullDateNextWeek.getDate()+"";if(twoDigitDateNextWeek.length==1)	twoDigitDateNextWeek="0" +twoDigitDateNextWeek;
+var todaysDate = fullDateToday.getFullYear() + "-" + twoDigitMonth + "-" + twoDigitDate;
+var nextWeeksDate = fullDateNextWeek.getFullYear() + "-" + twoDigitMonthNextweek + "-" + twoDigitDateNextWeek;
+
+window.app = new Vue ({
     el: '#app',
 
+    components: { datepicker },
+
     data: {
-        skills: []
+        momentDateTime: null,
+        
+        momentDate: null,
+
+        defaultDate: todaysDate,
+
+        dateSelected: todaysDate,
+
+        maxDate: nextWeeksDate,
+
+        featuredShow: {},
+        
+        tvSchedule: {},
+        
+        tvScheduleSelectedDay: {},
+
+        tvScheduleSelectedDate : '',
+
+        showFullSynopsis: false,
+
+        sched: '',
+
+        t: ''
     },
 
+    filters: {
+        truncate(string, value) {
+            return string.substring(0, value) + '...';
+        },
+
+    },
+    
+    methods: {
+        clear() {
+            this.init = ' '
+        },
+        updateEventDate(val) {
+            console.log(val)
+        },
+        dateSelect() {
+            this.featuredShow = this.tvSchedule[this.dateSelected].featured;
+            this.tvScheduleSelectedDay = this.tvSchedule[this.dateSelected].schedule;
+        },
+        fullSynopsis(show, event) {
+            this.$set(show, 'showFullSynopsis', true);
+            $('.tvShow').matchHeight();
+            $('html, body').animate({
+                scrollTop: $(event.srcElement).offset().top - 300
+            }, 1000);
+            console.log('fullSynopsis');
+            console.log(event);
+        },
+        shortSynopsis(show, event) {
+            this.$set(show, 'showFullSynopsis', false);
+            $('.tvShow').matchHeight();
+            $('html, body').animate({
+                scrollTop: $(event.srcElement).offset().top - 300
+            }, 1000);
+            console.log('shortSynopsis');
+            console.log(event);
+        },
+        addShowSynopsisVar(shows) {
+            for (var show = 0; show < shows.length; show++) {
+                this.$set(this.tvScheduleSelectedDay[show], 'showFullSynopsis', false);
+            }
+        },
+        momentClock() {
+            this.momentDate = moment(new Date());
+            this.momentDateTime = this.momentDate.format('dddd, MMMM Do YYYY, h:mm:ss a');
+        },
+        momentMidnight() {
+            setTimeout(function() {
+                console.log('reloading...');
+                document.location.reload(true);
+            }, moment("24:00:00", "hh:mm:ss").diff(moment(), 'milliseconds'));
+        }
+
+    },
+
+
     mounted() {
-        // make ajax reqeust /skills
-        this.$http.get('/skills').then(response => this.skills = response.data);
-    }
+        this.momentMidnight();
+        this.momentClock();
+        setInterval(this.momentClock, 1000);
+        // make ajax reqeust
+        this.$http.get('/tv-schedule').then(response => {
+            this.tvSchedule = response.data;
+
+            if (this.dateSelected) {
+                this.featuredShow = this.tvSchedule[this.dateSelected].featured;
+                this.tvScheduleSelectedDay = this.tvSchedule[this.dateSelected].schedule;
+                this.tvScheduleSelectedDate = this.tvSchedule[this.dateSelected].date_formatted;
+                this.addShowSynopsisVar(this.tvScheduleSelectedDay);
+            } else {
+                this.featuredShow = this.tvSchedule[this.defaultDate].featured;
+                this.tvScheduleSelectedDay = this.tvSchedule[this.defaultDate].schedule;
+                this.tvScheduleSelectedDate = this.tvSchedule[this.defaultDate].date_formatted;
+                this.addShowSynopsisVar(this.tvScheduleSelectedDay);
+            }
+
+            setTimeout(function() {
+                $('.tvShow').matchHeight();
+            }, 2000);
+        });
+    },
+
 });
--- resources/views/index.php
+++ resources/views/index.php
@@ -36,6 +36,15 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <style>
+        .picker {
+            width: 200px;
+        }
+        .tvShow {
+            padding-bottom: 10px;
+        }
+    </style>
   </head>
 
   <body>
@@ -44,46 +53,51 @@
       <div class="header clearfix">
         <nav>
           <ul class="nav nav-pills pull-right">
-            <li role="presentation" class="active"><a href="/">Home</a></li>
-            <li role="presentation" class="active">Select a Date <datepicker v-model="date"></datepicker></li>
+            <li role="presentation" class="active">
+                <h5>Select a Date:</h5> 
+                <datepicker v-model="dateSelected" class="picker" v-on:input="dateSelect" :min="defaultDate" :max="maxDate"></datepicker>
+            </li>
           </ul>
         </nav>
         <h3 class="text-muted">ION TV Schedule</h3>
+        <h5>Selected date: <b>{{ tvScheduleSelectedDate }}</b></h5>
+        <h5>Current date: <b>{{ momentDateTime }}</b></h5>
       </div>
 
       <div class="jumbotron">
-        <h1>Jumbotron heading</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
+        <h1>{{ featuredShow.showName }}</h1>
+        <img v-bind:src="featuredShow.image" v-bind:alt="featuredShow.showName" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        
+        <p class="lead"><strong>Episode:</strong> <br/> {{ featuredShow.episodetitle }}</p>
+        <p class="lead"><strong>Synopsis:</strong> <br/> {{ featuredShow.synopsis }}</p>
+        <p class="lead"><strong>Time:</strong> <br/> {{ featuredShow.tune_in_time }}</p>
+        <p class="lead"><strong>Duration:</strong> <br/> {{ featuredShow.duration / 60 }} Minutes</p>
 
-        <ul>
-            <!--<li v-for="skill in skills">@{{ skill }}</li>-->
-            <li v-for="skill in skills" v-text="skill"></li>
-        </ul>
+        <p><a class="btn btn-lg btn-primary" role="button" target="_blank" v-bind:href="featuredShow.link">View Show Info</a></p>
       </div>
 
       <div class="row marketing">
-        <div class="col-lg-6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+        <div class="tvShow col-lg-6 col-md-6 col-sm-6 col-xs-6" v-for="show in tvScheduleSelectedDay">
+          <img v-bind:src="show.image" v-bind:alt="show.showName" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h4>{{ show.showName }} - {{ show.episodetitle }}</h4>
+            
+            <h5>{{ show.tune_in_time }}</h5>
 
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+            <p class="synopsis-short" v-show="!show.showFullSynopsis">
+                <strong>Synopsis:</strong> {{ show.synopsis | truncate(100) }} 
+                <a @click="fullSynopsis(show, $event)">Show More</a>
+            </p>
+            <p class="synopsis-full" v-show="show.showFullSynopsis">
+                <strong>Synopsis:</strong> {{ show.synopsis }}
+                <a @click="shortSynopsis(show, $event)">Show Less</a>
+            </p>
+            
+            <a target="_blank" v-bind:href="show.link">View Show Info</a>
+            <p>&nbsp;</p>
+          </div>
         </div>
 
-        <div class="col-lg-6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
       </div>
 
       <footer class="footer">
@@ -94,8 +108,13 @@
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    
-    <!-- Latest compiled and minified JavaScript -->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.matchHeight/0.7.0/jquery.matchHeight-min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/later/1.2.0/later.min.js"></script>
+
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
