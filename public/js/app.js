5f0f14851a79258601965a9f0096dcdce9d7d319
Steven Gonzalez <uniacid@gmail.com>
2017-06-03 23:43:19 -0400

    Add promise polyfill and convert to es5 code to work in IE

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 Vue.prototype.$http = axios;
 
 var fullDateToday = new Date();
@@ -5,23 +7,23 @@ var fullDateTomorrow = new Date();
 var fullDateNextWeek = new Date();
 var d = new Date();
 fullDateTomorrow.setDate(fullDateTomorrow.getDate() + 1);
-fullDateTomorrow.setHours(0,0,0,0);
+fullDateTomorrow.setHours(0, 0, 0, 0);
 fullDateNextWeek.setDate(fullDateNextWeek.getDate() + 7);
-var twoDigitMonth = fullDateToday.getMonth()+1+"";if(twoDigitMonth.length==1)	twoDigitMonth="0" +twoDigitMonth;
-var twoDigitMonthNextweek = fullDateToday.getMonth()+1+"";if(twoDigitMonthNextweek.length==1)	twoDigitMonthNextweek="0" +twoDigitMonthNextweek;
-var twoDigitDate = fullDateToday.getDate()+"";if(twoDigitDate.length==1)	twoDigitDate="0" +twoDigitDate;
-var twoDigitDateNextWeek = fullDateNextWeek.getDate()+"";if(twoDigitDateNextWeek.length==1)	twoDigitDateNextWeek="0" +twoDigitDateNextWeek;
+var twoDigitMonth = fullDateToday.getMonth() + 1 + "";if (twoDigitMonth.length == 1) twoDigitMonth = "0" + twoDigitMonth;
+var twoDigitMonthNextweek = fullDateToday.getMonth() + 1 + "";if (twoDigitMonthNextweek.length == 1) twoDigitMonthNextweek = "0" + twoDigitMonthNextweek;
+var twoDigitDate = fullDateToday.getDate() + "";if (twoDigitDate.length == 1) twoDigitDate = "0" + twoDigitDate;
+var twoDigitDateNextWeek = fullDateNextWeek.getDate() + "";if (twoDigitDateNextWeek.length == 1) twoDigitDateNextWeek = "0" + twoDigitDateNextWeek;
 var todaysDate = fullDateToday.getFullYear() + "-" + twoDigitMonth + "-" + twoDigitDate;
 var nextWeeksDate = fullDateNextWeek.getFullYear() + "-" + twoDigitMonthNextweek + "-" + twoDigitDateNextWeek;
 
-window.app = new Vue ({
+window.app = new Vue({
     el: '#app',
 
-    components: { datepicker },
+    components: { datepicker: datepicker },
 
     data: {
         momentDateTime: null,
-        
+
         momentDate: null,
 
         defaultDate: todaysDate,
@@ -31,12 +33,12 @@ window.app = new Vue ({
         maxDate: nextWeeksDate,
 
         featuredShow: {},
-        
+
         tvSchedule: {},
-        
+
         tvScheduleSelectedDay: {},
 
-        tvScheduleSelectedDate : '',
+        tvScheduleSelectedDate: '',
 
         showFullSynopsis: false,
 
@@ -46,24 +48,23 @@ window.app = new Vue ({
     },
 
     filters: {
-        truncate(string, value) {
+        truncate: function truncate(string, value) {
             return string.substring(0, value) + '...';
-        },
-
+        }
     },
-    
+
     methods: {
-        clear() {
-            this.init = ' '
+        clear: function clear() {
+            this.init = ' ';
         },
-        updateEventDate(val) {
-            console.log(val)
+        updateEventDate: function updateEventDate(val) {
+            console.log(val);
         },
-        dateSelect() {
+        dateSelect: function dateSelect() {
             this.featuredShow = this.tvSchedule[this.dateSelected].featured;
             this.tvScheduleSelectedDay = this.tvSchedule[this.dateSelected].schedule;
         },
-        fullSynopsis(show, event) {
+        fullSynopsis: function fullSynopsis(show, event) {
             this.$set(show, 'showFullSynopsis', true);
             $('.tvShow').matchHeight();
             $('html, body').animate({
@@ -72,7 +73,7 @@ window.app = new Vue ({
             console.log('fullSynopsis');
             console.log(event);
         },
-        shortSynopsis(show, event) {
+        shortSynopsis: function shortSynopsis(show, event) {
             this.$set(show, 'showFullSynopsis', false);
             $('.tvShow').matchHeight();
             $('html, body').animate({
@@ -81,49 +82,48 @@ window.app = new Vue ({
             console.log('shortSynopsis');
             console.log(event);
         },
-        addShowSynopsisVar(shows) {
+        addShowSynopsisVar: function addShowSynopsisVar(shows) {
             for (var show = 0; show < shows.length; show++) {
                 this.$set(this.tvScheduleSelectedDay[show], 'showFullSynopsis', false);
             }
         },
-        momentClock() {
+        momentClock: function momentClock() {
             this.momentDate = moment(new Date());
             this.momentDateTime = this.momentDate.format('dddd, MMMM Do YYYY, h:mm:ss a');
         },
-        momentMidnight() {
-            setTimeout(function() {
+        momentMidnight: function momentMidnight() {
+            setTimeout(function () {
                 console.log('reloading...');
                 document.location.reload(true);
             }, moment("24:00:00", "hh:mm:ss").diff(moment(), 'milliseconds'));
         }
-
     },
 
+    mounted: function mounted() {
+        var _this = this;
 
-    mounted() {
         this.momentMidnight();
         this.momentClock();
         setInterval(this.momentClock, 1000);
         // make ajax reqeust
-        this.$http.get('tv-schedule').then(response => {
-            this.tvSchedule = response.data;
+        this.$http.get('tv-schedule').then(function (response) {
+            _this.tvSchedule = response.data;
 
-            if (this.dateSelected) {
-                this.featuredShow = this.tvSchedule[this.dateSelected].featured;
-                this.tvScheduleSelectedDay = this.tvSchedule[this.dateSelected].schedule;
-                this.tvScheduleSelectedDate = this.tvSchedule[this.dateSelected].date_formatted;
-                this.addShowSynopsisVar(this.tvScheduleSelectedDay);
+            if (_this.dateSelected) {
+                _this.featuredShow = _this.tvSchedule[_this.dateSelected].featured;
+                _this.tvScheduleSelectedDay = _this.tvSchedule[_this.dateSelected].schedule;
+                _this.tvScheduleSelectedDate = _this.tvSchedule[_this.dateSelected].date_formatted;
+                _this.addShowSynopsisVar(_this.tvScheduleSelectedDay);
             } else {
-                this.featuredShow = this.tvSchedule[this.defaultDate].featured;
-                this.tvScheduleSelectedDay = this.tvSchedule[this.defaultDate].schedule;
-                this.tvScheduleSelectedDate = this.tvSchedule[this.defaultDate].date_formatted;
-                this.addShowSynopsisVar(this.tvScheduleSelectedDay);
+                _this.featuredShow = _this.tvSchedule[_this.defaultDate].featured;
+                _this.tvScheduleSelectedDay = _this.tvSchedule[_this.defaultDate].schedule;
+                _this.tvScheduleSelectedDate = _this.tvSchedule[_this.defaultDate].date_formatted;
+                _this.addShowSynopsisVar(_this.tvScheduleSelectedDay);
             }
 
-            setTimeout(function() {
+            setTimeout(function () {
                 $('.tvShow').matchHeight();
             }, 2000);
         });
-    },
-
+    }
 });
